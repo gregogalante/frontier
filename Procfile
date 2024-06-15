3aa03e51fdd12ba832d6fc1f90c0f8a55bbf192e
genjigakura <dev@uomi.ai>
2024-06-15 13:42:11 +0200

    Make js to manage transaction from alith to main account

--- Procfile
+++ Procfile
@@ -1,5 +1,3 @@
 alice: ./target/release/frontier-template-node purge-chain --base-path /tmp/alice --chain local -y && ./target/release/frontier-template-node --base-path /tmp/alice --chain local --alice --port 30333 --rpc-port 9945 --node-key 0000000000000000000000000000000000000000000000000000000000000001 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator
 
 bob: sleep 5 && ./target/release/frontier-template-node purge-chain --base-path /tmp/bob --chain local -y && ./target/release/frontier-template-node --base-path /tmp/bob --chain local --bob --port 30334 --rpc-port 9946 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
-
-_devclients: cd _devclients && yarn install && yarn start
--- _devclients/index.js
+++ _devclients/index.js
@@ -2,27 +2,31 @@ import { ethers } from "ethers"
 
 
 
-const ACCOUNT_PUBLIC_KEY = '0x80b5F94B64871BdFbE9b581aF4360D1D5311C6B5'
-const ACCOUNT_PRIVATE_KEY = '356d84478609ba076181ba09d5dc227dd754cee5cdb649fbc28c0dd44fbd3e44'
+const MAIN_PUBLIC_KEY = '0x80b5F94B64871BdFbE9b581aF4360D1D5311C6B5'
+const MAIN_PRIVATE_KEY = '356d84478609ba076181ba09d5dc227dd754cee5cdb649fbc28c0dd44fbd3e44'
 
 const ALITH_PUBLIC_KEY = '0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac'
+const ALITH_PRIVATE_KEY = '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133'
 
 const provider = new ethers.JsonRpcProvider("http://localhost:9945")
-const account = new ethers.Wallet(ACCOUNT_PRIVATE_KEY, provider)
+const mainAccount = new ethers.Wallet(MAIN_PRIVATE_KEY, provider)
+const alithAccount = new ethers.Wallet(ALITH_PRIVATE_KEY, provider)
 
-let balance = 0
-while (true) {
-  balance = await provider.getBalance(ACCOUNT_PUBLIC_KEY)
-  console.log('Balance:', ethers.formatEther(balance))
-  // if (balance > 0) {
-  //   const tx = await account.sendTransaction({
-  //     to: ALITH_PUBLIC_KEY,
-  //     value: ethers.parseEther('0'),
-  //     data: ethers.toUtf8Bytes('Hello Alith'),
-  //     gasPrice: ethers.parseUnits('200', 'gwei'),
-  //     gasLimit: 21000
-  //   })
-  //   console.log('Transaction:', tx.hash)
-  // }
-  await new Promise(resolve => setTimeout(resolve, 1000))
+const mainBalance = await provider.getBalance(MAIN_PUBLIC_KEY)
+console.log('Main Balance:', ethers.formatEther(mainBalance))
+
+const alithBalance = await provider.getBalance(ALITH_PUBLIC_KEY)
+console.log('Alith Balance:', ethers.formatEther(alithBalance))
+
+const _ = async () => {
+  if (mainBalance <= 0) {
+    // send half of alith balance to main account
+    const amount = alithBalance / BigInt(2)
+    const tx = await alithAccount.sendTransaction({
+      to: MAIN_PUBLIC_KEY,
+      value: amount
+    })
+    console.log('Transaction:', tx)
+  }
 }
+_()
