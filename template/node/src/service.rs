d69a8c6b5b70823f433b2a002c6f896d68f3caa0
gregogalante <me@gregoriogalante.com>
2024-07-03 13:54:57 +0200

    Cleanup + added node code

--- template/node/src/service.rs
+++ template/node/src/service.rs
@@ -18,6 +18,31 @@ use sp_api::ConstructRuntimeApi;
 use sp_consensus_aura::sr25519::{AuthorityId as AuraId, AuthorityPair as AuraPair};
 use sp_core::{H256, U256};
 use sp_runtime::traits::{Block as BlockT, NumberFor};
+
+// CONTRACTS PALLET
+////////////////////////////////////////////////////////////////////////////////
+
+pub use sc_executor::NativeElseWasmExecutor;
+pub struct ExecutorDispatch;
+impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
+	/// Only enable the benchmarking host functions when we actually want to benchmark.
+	#[cfg(feature = "runtime-benchmarks")]
+	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
+	/// Otherwise we only use the default Substrate host functions.
+	#[cfg(not(feature = "runtime-benchmarks"))]
+	type ExtendHostFunctions = ();
+
+	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
+		frontier_template_runtime::api::dispatch(method, data)
+	}
+
+	fn native_version() -> sc_executor::NativeVersion {
+		frontier_template_runtime::native_version()
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 // Runtime
 use frontier_template_runtime::{
 	opaque::Block, AccountId, Balance, Nonce, RuntimeApi, TransactionConverter,
